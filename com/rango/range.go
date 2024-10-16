27677c4f223545c395975e27a57acd90d5a81c7f
兰戈 <2380584338@qq.com>
2024-10-16 17:55:05 +0800

    learn range

--- com/rango/range.go
+++ com/rango/range.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import "fmt"
 
+var rangePow = []int{1, 2, 4, 8, 16, 32, 64, 128}
+
+func main() {
+	for i, v := range rangePow {
+		fmt.Printf("2**%d = %d\n", i, v)
+	}
+
+	for i := range rangePow {
+		fmt.Println(i)
+	}
+
+	for _, v := range rangePow {
+		fmt.Println(v)
+	}
 }
