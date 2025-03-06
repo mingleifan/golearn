9c0f893175160272363d9cbca01ba61c522a4ff5
兰戈 <2380584338@qq.com>
2025-03-06 15:51:01 +0800

    250306: import extension

--- com/25/4-init/main.go
+++ com/25/4-init/main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"golearn/com/25/4-init/lib1"
-	"golearn/com/25/4-init/lib2"
+	mylib2 "golearn/com/25/4-init/lib2"
+	//匿名导入包但并不使用 -> [_ "golearn/com/25/4-init/lib2"]
+	//导入包别名 -> [myLib2 "golearn/com/25/4-init/lib2"]
+	//导入包为当前main包的方法 -> [. "golearn/com/25/4-init/lib2"] -> 就不需要 包名.func, 直接使用func ex:尽量不使用
 )
 
 func main() {
 	lib1.FuncLib1()
-	lib2.FuncLib2()
+	mylib2.FuncLib2()
 }
