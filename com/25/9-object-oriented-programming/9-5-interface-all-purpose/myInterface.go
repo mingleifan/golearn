ca93626d5baa5a1e5eb4e6b2b6f7df03ba8e0c5a
兰戈 <2380584338@qq.com>
2025-03-10 14:05:24 +0800

    250310: interface{} 万能类型

--- com/25/9-object-oriented-programming/9-5-interface-all-purpose/myInterface.go
+++ com/25/9-object-oriented-programming/9-5-interface-all-purpose/myInterface.go
@@ -2,10 +2,12 @@ package main
 
 import "fmt"
 
+// interface{} 万能数据类型
 func myFunc(arg interface{}) {
 	fmt.Println("myFunc is called...")
 	fmt.Println(arg)
 	//interface{}该如何区分 此时引用的底层数据类型是什么？
+	//给 interface{}提供 “断言“
 	value, ok := arg.(string)
 	if ok {
 		fmt.Println("arg is string type")
